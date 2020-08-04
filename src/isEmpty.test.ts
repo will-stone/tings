319df97729798e53e7ea07c3ff5af04de237cde1
Will Stone <email@wstone.io>
2020-08-04 17:25:05 +0100

    Test isEmpty and isNotEmpty

---
+++ src/isEmpty.test.ts
@@ -0,0 +1,9 @@
+import isEmpty from './isEmpty'
+
+test.each(['has content', '\r\n\t\f\v'])('isEmpty(%p) is false', (input) => {
+  expect(isEmpty(input)).toBe(false)
+})
+
+test.each([''])('isEmpty(%p) is true', (input) => {
+  expect(isEmpty(input)).toBe(true)
+})
---
+++ src/isNotEmpty.test.ts
@@ -0,0 +1,9 @@
+import isNotEmpty from './isNotEmpty'
+
+test.each([''])('isNotEmpty(%p) is false', (input) => {
+  expect(isNotEmpty(input)).toBe(false)
+})
+
+test.each(['has content', '\r\n\t\f\v'])('isNotEmpty(%p) is true', (input) => {
+  expect(isNotEmpty(input)).toBe(true)
+})
