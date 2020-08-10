bde476662c17ab19b77d16a3d849ade7262f2c30
Will Stone <email@wstone.io>
2020-08-10 13:12:36 +0100

    More camelCase test cases

--- src/isCamelCase.test.ts
+++ src/isCamelCase.test.ts
@@ -9,11 +9,12 @@ test.each([
   '123',
   'containsSpecial!Characters',
   '!',
+  'thisHas aSpace',
 ])('isCamelCase(%p) is false', (input) => {
   expect(isCamelCase(input)).toBe(false)
 })
 
-test.each(['one', 'a', 'camelCase', 'miltiWordCamelCase'])(
+test.each(['one', 'a', 'camelCase', 'miltiWordCamelCase', 'aWord'])(
   'isCamelCase(%p) is true',
   (input) => {
     expect(isCamelCase(input)).toBe(true)
