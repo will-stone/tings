ba4bcad3b5d92ec04a9a674e409c1653b0a97473
Will Stone <email@wstone.io>
2020-08-10 13:13:25 +0100

    Add camelCase to common tests

--- src/_common.test.ts
+++ src/_common.test.ts
@@ -1,27 +1,31 @@
+import isCamelCase from './isCamelCase'
 import isCompact from './isCompact'
 import isEmpty from './isEmpty'
 import isNotEmpty from './isNotEmpty'
 
-describe.each([isCompact, isEmpty, isNotEmpty])('%p', (validator) => {
-  test.each(['string'])('not throw on %p', (input) => {
-    expect(() => validator(input)).not.toThrow()
-  })
+describe.each([isCompact, isEmpty, isNotEmpty, isCamelCase])(
+  '%p',
+  (validator) => {
+    test.each(['string'])('not throw on %p', (input) => {
+      expect(() => validator(input)).not.toThrow()
+    })
 
-  test.each([
-    0,
-    1,
-    new Date(),
-    undefined,
-    true,
-    false,
-    {},
-    [],
-    BigInt(1),
-    Symbol(1),
-    () => '',
-  ])('throw on %p', (input) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    expect(() => validator(input)).toThrow('Input is not a string')
-  })
-})
+    test.each([
+      0,
+      1,
+      new Date(),
+      undefined,
+      true,
+      false,
+      {},
+      [],
+      BigInt(1),
+      Symbol(1),
+      () => '',
+    ])('throw on %p', (input) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      expect(() => validator(input)).toThrow('Input is not a string')
+    })
+  },
+)
