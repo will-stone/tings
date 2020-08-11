7e740e16435d695692b56cd3cb4c4c9b965d5e0a
Will Stone <email@wstone.io>
2020-08-11 16:32:17 +0100

    Improve test speed

--- tests/_common.ts
+++ tests/_common.ts
@@ -1,20 +1,11 @@
-import fs from 'fs'
-import path from 'path'
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-const validators: NodeRequire[] = []
+import * as all from '../src'
 
-const sourceDirectory = path.join(__dirname, '..', 'src')
+const validators = Object.entries(all).filter(([name]) => name.startsWith('is'))
 
-fs.readdirSync(sourceDirectory).forEach((file) => {
-  if (file.startsWith('is')) {
-    // eslint-disable-next-line node/global-require, @typescript-eslint/no-var-requires
-    validators.push(require(path.join(sourceDirectory, file)).default)
-  }
-})
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- allow all bad types
 const invalidInputs: any[] = [
   0,
   1,
@@ -29,13 +20,13 @@ const invalidInputs: any[] = [
   () => '',
 ]
 
-validators.forEach((validator) => {
-  test('not throw on string', () => {
+validators.forEach(([name, validator]) => {
+  test(`${name} does not throw on string`, () => {
     assert.not.throws(() => validator('string'))
   })
 
   invalidInputs.forEach((input) => {
-    test(`throws ${String(input)} is not a string`, () => {
+    test(`${name} throws ${String(input)} is not a string`, () => {
       assert.throws(
         () => validator(input),
         (error: Error) => error.message === 'Input is not a string',
