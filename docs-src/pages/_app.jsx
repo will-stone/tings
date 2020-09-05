0f77da5d8b0fd84fdcc9b7b62aafcf1dff86f22a
Will Stone <email@wstone.io>
2020-09-05 21:53:35 +0100

    Add favicon and title

--- docs-src/pages/_app.jsx
+++ docs-src/pages/_app.jsx
@@ -1,10 +1,39 @@
 import '../styles/index.css'
 
+import Head from 'next/head'
 import React from 'react'
 
 function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>tings</title>
+
+        <link
+          href="/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/site.webmanifest" rel="manifest" />
+        <link color="#cb1b71" href="/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="#ffffff" name="msapplication-TileColor" />
+        <meta content="#ffffff" name="theme-color" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
