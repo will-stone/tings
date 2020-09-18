c3234bf9f082da5d13bc3a638939fc12fe270112
Will Stone <email@wstone.io>
2020-09-18 07:54:34 +0100

    Fix massive github icon

--- docs-src/pages/_app.jsx
+++ docs-src/pages/_app.jsx
@@ -1,7 +1,12 @@
 import '../styles/index.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
+import { config as fontawesomeConfig } from '@fortawesome/fontawesome-svg-core'
 import Head from 'next/head'
 import React from 'react'
+
+// Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+fontawesomeConfig.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
