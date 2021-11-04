db867769edc8c19238ba79bd6e3e029d6cd47778
Will Stone <654103+will-stone@users.noreply.github.com>
2021-11-04 11:48:12 +0000

    Error on import from index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export { default as isCompactCase } from './isCompactCase'
-export { default as isLowerCase } from './isLowerCase'
-export { default as isUniq } from './isUniq'
-export { default as sleep } from './sleep'
-export { default as toCompactCase } from './toCompactCase'
-export { default as toLetters } from './toLetters'
-export { default as toNumberAlways } from './toNumberAlways'
-export { default as toOrdinal } from './toOrdinal'
+// eslint-disable-next-line no-console
+console.error('Tings should be imported directly')
+
+export {}
