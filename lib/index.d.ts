4dede8225b696ccdd4f1fcde337f202405853146
Will Stone <654103+will-stone@users.noreply.github.com>
2021-12-25 13:04:32 +0000

    Out to lib and add to git

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export { default as isCompactCase } from './isCompactCase';
+export { default as isLowerCase } from './isLowerCase';
+export { default as isUniq } from './isUniq';
+export { default as sleep } from './sleep';
+export { default as toCompactCase } from './toCompactCase';
+export { default as toLetters } from './toLetters';
+export { default as toNumberAlways } from './toNumberAlways';
+export { default as toOrdinal } from './toOrdinal';
---
+++ lib/toNumberAlways.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Will return the given input as a number, if it cannot be converted, it will return 0.
+ * @example
+```ts
+import toNumberAlways from 'tings/toNumberAlways'
+
+toNumberAlways('this is text') // 0
+toNumberAlways('3') // 3
+toNumberAlways(0.4) // 0.4
+toNumberAlways(new Set([])) // 0
+```
+ */
+declare function toNumberAlways(input: unknown): number;
+export default toNumberAlways;
