b73edf96dd94fa4ba7fd2f8f4a980fda549be392
Will Stone <654103+will-stone@users.noreply.github.com>
2021-12-26 19:44:15 +0000

    Add isBrowser ting

---
+++ lib/isBrowser.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Is current code running in a browser environment
+ * @example
+```ts
+import isBrowser from 'tings/lib/isBrowser'
+
+isBrowser() // true
+```
+ */
+declare function isBrowser(): boolean;
+export default isBrowser;
