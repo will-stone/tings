9aa9b1a7299a651859796103a26a5d096f5e49d8
Will Stone <654103+will-stone@users.noreply.github.com>
2022-11-11 07:27:20 +0000

    Move tests inline with source

--- package/common.test.ts
+++ package/common.test.ts
@@ -2,10 +2,10 @@ import fs from 'fs'
 import path from 'path'
 import { expect, test } from 'vitest'
 
-import * as all from '../dist/index.js'
+import * as all from './source/index.js'
 
 const tingFileNames = fs
-  .readdirSync(path.join(__dirname, '..', 'source'))
+  .readdirSync(path.join(__dirname, 'source'))
   .filter(
     (n) =>
       // Do not count the index file
@@ -13,11 +13,6 @@ const tingFileNames = fs
       // Do not count hidden files, e.g. .DS_Store
       !n.startsWith('.'),
   )
-  .map((f) => f.split('.')[0])
-
-const tingTestFileNames = fs
-  .readdirSync(__dirname)
-  .filter((n) => n !== '_common.test.ts')
   .map((f) => f.split('.')[0])
 
 const camelToKebab = (string_: string): string =>
@@ -28,9 +23,4 @@ const tings = Object.keys(all).map(camelToKebab)
 test('index exports all tings', () => {
   expect(tingFileNames.length).toBe(tings.length)
   expect(tingFileNames).toEqual(tings)
-})
-
-test('all tings have a test file', () => {
-  expect(tingTestFileNames.length).toBe(tings.length)
-  expect(tingTestFileNames).toEqual(tings)
 })
--- package/source/is-lower-case.ts
+++ package/source/is-lower-case.ts
@@ -8,14 +8,34 @@ isLowerCase('this is lower') // true
 isLowerCase('This is not LOWER') // false
 ```
  */
-function isLowerCase(string: string): boolean {
-  const input = string as unknown
-
+function isLowerCase(input: unknown): input is string {
   if (typeof input !== 'string') {
     return false
   }
 
   return input === input.toLowerCase()
+}
+
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest
+
+  const falsey = ['Title Case', 'camelCase', 'PascalCase', []]
+
+  test.each(falsey)('%s is false', (input) => {
+    expect(isLowerCase(input)).toBe(false)
+  })
+
+  const truthy = [
+    'lowercase',
+    'lower case',
+    '',
+    `
+`,
+  ]
+
+  test.each(truthy)('%s is true', (input) => {
+    expect(isLowerCase(input)).toBe(true)
+  })
 }
 
 export { isLowerCase }
