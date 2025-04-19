6980963f5c77fdffd60053c454bae18f279a088f
Will Stone <654103+will-stone@users.noreply.github.com>
2025-04-19 19:37:37 +0100

    Upgrade linting and formatting

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,5 @@ import prettierConfig from '@will-stone/prettier-config'
 
 export default {
   ...prettierConfig,
-  plugins: [...prettierConfig.plugins, 'prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro'],
 }
